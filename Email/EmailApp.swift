161c39f290b67a6a6fe5f1698213a8b5a5978e14
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-29 11:58:32 -0600

    Initial Commit

---
+++ Email/EmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailApp.swift
+//  Email
+//
+//  Created by Yael Javier Zamora Moreno on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
